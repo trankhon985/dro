2eed758f29bc997b1e89327d5f604b15978cead9
trankhon985 <165470460+trankhon985@users.noreply.github.com>
2024-03-30 17:53:34 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+
+while :; do
+
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqculg9r3zqds8hps -r community-pools.mysrv.cloud:10300 -p rpc;
+    
+    sleep 5;
+
+done
